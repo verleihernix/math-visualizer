f0527a7b32c9a16b980a755645f89db081938a1d
verleihernix <mikazernikow24@gmail.com>
2026-02-14 19:19:14 +0100

    add value calculation (v0.1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
 #include "console/Console.hpp"
+#include "parser/ExpressionParser.hpp"
+#include <iomanip>
+#include <thread>
 
 int main()
 {
-	console::open("Initial");
+	console::open("Test");
+
+	std::string expression = console::input("Enter a function (e.g. sin(x) * x^2)", true);
+
+	try
+	{
+		auto func = parser::parseExpression(expression);
+
+		console::print(console::Color::Yellow, true, "\nTable of values:");
+		console::print(console::Color::White, true, "------------------------------------------------------------------");
+
+		console::print(console::Color::Cyan, true,
+			std::setw(10), "x",
+			std::setw(10), "f(x)"
+		);
+
+		console::print(console::Color::White, true, "------------------------------------------------------------------");
+
+		for (float x = -5.f; x <= 5.f; x += 1.f)
+		{
+			float y = func(x);
+
+			console::print(
+				console::Color::White,
+				true,
+				std::setw(10), x,
+				std::setw(15), y
+			);
+		}
+	}
+	catch (const std::exception& e)
+	{
+		console::print(console::Color::Red, true, "\n Error while parsing:");
+		console::print(console::Color::Red, true, e.what());
+	}
+
+	std::this_thread::sleep_for(std::chrono::seconds(10));
+
+	return 0;
 }
